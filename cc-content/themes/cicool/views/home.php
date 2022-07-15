eb79261135b0ab77d96f1a8692e93d7621739a83
suryo <suryoatm@gmail.com>
2022-07-15 09:21:13 +0700

    upload  :   captcha & cc-content folders

---
+++ cc-content/themes/cicool/views/home.php
@@ -0,0 +1,15 @@
+<?= get_header(); ?>
+
+<body id="page-top">
+   <?= get_navigation(); ?>
+
+   <header>
+      <div class="header-content">
+         <div class="header-content-inner">
+            <h1 id="homeHeading">WELCOME TO PANDANGARDEN</h1>
+            <hr>
+            
+         </div>
+      </div>
+   </header>
+   <?= get_footer(); ?>
