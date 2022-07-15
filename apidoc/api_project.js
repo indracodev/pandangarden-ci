508fa6eae801cc42f978b62510377c1f3b2188a5
suryo <suryoatm@gmail.com>
2022-07-15 09:13:24 +0700

    upload  :   apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "PAndan Garden Documentation",
+  "version": "0.1.0",
+  "description": "API DOC Pandangarden",
+  "title": "PAndan Garden application",
+  "url": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-10-29T12:51:55.712Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
